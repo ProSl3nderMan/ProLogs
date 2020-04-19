894a826f73cc4462ab979491ffb0ee55846929ae
ProSl3nderMan <austinbworkin@gmail.com>
2020-04-19 02:10:09 -0500

    Main class and plugin.yml creation

---
+++ src/main/java/me/prosl3nderman/prologs/ProLogs.java
@@ -0,0 +1,17 @@
+package me.prosl3nderman.prologs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProLogs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
